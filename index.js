1332789958e01035a70148cc238d37fae2e34bf7
Romina Lodolo <romina@flume.co.za>
2026-05-13 12:01:43 +0200

    testing animations

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+let box = document.querySelector(".box");
+let circle = document.querySelector(".circle");
+
 // tweening 
-gsap.to(".box", {
-    y: 200,
+gsap.to([box, circle], {
+    x: 100,
+    y: 30,
+    
     duration: 1,
     ease: "bounce.out",
     delay: 0.5
 })
+
